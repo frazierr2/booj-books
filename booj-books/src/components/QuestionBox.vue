bbcf15423c4fe720f65824381c3ee82b20ce6484
Ryan Frazier <frazierr2@gmail.com>
2019-05-10 11:13:36 -0400

    finished quiz app

--- booj-books/src/components/QuestionBox.vue
+++ booj-books/src/components/QuestionBox.vue
@@ -7,39 +7,87 @@
 
       <b-list-group>
         <b-list-group-item
-          v-for="(answer,index) in answers"
+          v-for="(answer,index) in shuffledAnswers"
           :key="index"
-          @click="selectAnswer(index)"
+          @click.prevent="selectAnswer(index)"
+          :class="answerClass(index)"
         >{{ answer }}</b-list-group-item>
       </b-list-group>
 
-      <b-button variant="primary" href="#">Submit</b-button>
+      <b-button
+        variant="primary"
+        @click="submitAnswer"
+        :disabled="selectedIndex === null || justAnswered"
+      >Submit</b-button>
       <b-button variant="success" href="#" @click="next">Next</b-button>
     </b-jumbotron>
   </div>
 </template>
 
 <script>
+import _ from "lodash";
+
 export default {
   props: {
     currentQuestion: Object,
-    next: Function
+    next: Function,
+    increment: Function
   },
   data() {
     return {
-      selectedIndex: null
+      selectedIndex: null,
+      correctIndex: null,
+      shuffledAnswers: [],
+      justAnswered: false
     };
+  },
+  watch: {
+    currentQuestion: {
+      immediate: true,
+      handler() {
+        this.selectedIndex = null;
+        this.shuffleAnswers();
+        this.justAnswered = false;
+      }
+    }
   },
   methods: {
     selectAnswer(index) {
       this.selectedIndex = index;
-    }
-  },
-  computed: {
-    answers() {
-      let answers = [...this.currentQuestion.incorrect_answers]; //Using this in computed because it's coming from prop
-      answers.push(this.currentQuestion.correct_answer);
-      return answers;
+    },
+    shuffleAnswers() {
+      let answers = [
+        ...this.currentQuestion.incorrect_answers,
+        this.currentQuestion.correct_answer
+      ];
+      this.shuffledAnswers = _.shuffle(answers);
+      this.correctIndex = this.shuffledAnswers.indexOf(
+        this.currentQuestion.correct_answer
+      );
+    },
+    submitAnswer() {
+      let isCorrect = false;
+      if (this.selectedIndex === this.correctIndex) {
+        isCorrect = true;
+      }
+      this.justAnswered = true;
+      this.increment(isCorrect);
+    },
+    answerClass(index) {
+      let answerClass = "";
+
+      if (!this.justAnswered && this.selectedIndex === index) {
+        answerClass = "selected";
+      } else if (this.justAnswered && this.correctIndex === index) {
+        answerClass = "correct";
+      } else if (
+        this.justAnswered &&
+        this.selectedIndex === index &&
+        this.correctIndex !== index
+      ) {
+        answerClass = "incorrect";
+      }
+      return answerClass;
     }
   }
 };
@@ -52,7 +100,20 @@ export default {
 .list-group-item {
   margin-bottom: 2px;
 }
+.list-group-item:hover {
+  background: #eee;
+  cursor: pointer;
+}
 .btn {
   margin: 0 5px;
+}
+.selected {
+  background-color: lightblue;
+}
+.correct {
+  background-color: lightgreen;
+}
+.incorrect {
+  background-color: red;
 }
 </style>
